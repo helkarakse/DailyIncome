b440ab63e39249b88b956cde5f6bb3d94d5dc4e0
helkarakse <nimcuron@gmail.com>
2013-12-17 07:57:12 +0800

    Created the player listener class for daily income

---
+++ src/net/helkarakse/dailyincome/DailyIncomePlayerListener.java
@@ -0,0 +1,11 @@
+package net.helkarakse.dailyincome;
+
+import org.bukkit.event.Listener;
+
+public class DailyIncomePlayerListener implements Listener {
+	private DailyIncome plugin;
+	
+	public DailyIncomePlayerListener(DailyIncome instance) {
+		plugin = instance;
+	}
+}
