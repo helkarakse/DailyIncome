ecc8136994910a13eb2195247c63f5ce1d083bb0
helkarakse <nimcuron@gmail.com>
2013-12-17 07:40:46 +0800

    Added the sqlite lib and the code to init the db onEnable

--- src/net/helkarakse/dailyincome/DailyIncome.java
+++ src/net/helkarakse/dailyincome/DailyIncome.java
@@ -1,7 +1,31 @@
 package net.helkarakse.dailyincome;
 
+import java.util.logging.Logger;
+
+import lib.PatPeter.SQLibrary.Database;
+import lib.PatPeter.SQLibrary.SQLite;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DailyIncome extends JavaPlugin {
-	
+	private Database db;
+	private Logger logger = Logger.getLogger("DailyIncome");
+
+	@Override
+	public void onEnable() {
+		db = new SQLite(logger, "[DailyIncome] ", getDataFolder()
+				.getAbsolutePath(), "DailyIncome", ".sqlite");
+
+		try {
+			db.open();
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+			getPluginLoader().disablePlugin(this);
+		}
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
 }
